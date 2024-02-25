dc65c74d6d599075bb497d7c55b32b5142c46a67
RUNFUNRUN <ryotauchiyama1212@gmail.com>
2024-02-25 19:42:21 +0900

    fix style and words

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -7,9 +7,9 @@ const archivoBlack = Archivo_Black({ weight: ['400'], subsets: ['latin'] });
 
 export default function NotFound() {
   return (
-    <main>
+    <main className='text-center'>
       <h2 className={`text-4xl sm:text-6xl lg:text-8xl ${archivoBlack.className}`}>Not Found</h2>
-      <p className='text-sm sm:text-xl lg:text-2xl'>Could not find requested resource</p>
+      <p className='text-sm sm:text-xl lg:text-2xl'>This is ramen.</p>
       <Image src='/ramen.jpg' width={800} height={600} alt='yoshimueraya' className='mx-auto' />
       <Link href='/'>
         <Button color='secondary' size='lg' className='my-3 sm:my-6 lg:my-9 w-40'>
