ca946805b2fca9957c242a1da5aa40cab8d6c05a
RUNFUNRUN <ryotauchiyama1212@gmail.com>
2024-12-13 00:13:57 +0900

    fix: remove GoogleAnalytics scripts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import { ThemeProvider } from '@/components/theme-provider';
 import { Toaster } from '@/components/ui/sonner';
 import { cn } from '@/lib/utils';
-import { GoogleAnalytics } from '@next/third-parties/google';
 import type { Metadata } from 'next';
 import { Source_Code_Pro } from 'next/font/google';
 import type { ReactNode } from 'react';
@@ -18,9 +17,6 @@ const RootLayout = ({
 }>) => {
   return (
     <html lang='en'>
-      {process.env.NODE_ENV === 'production' && (
-        <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID ?? ''} />
-      )}
       <body className={cn('flex min-h-dvh flex-col', sourceCodePro.className)}>
         <ThemeProvider attribute='class' defaultTheme='dark'>
           {children}
